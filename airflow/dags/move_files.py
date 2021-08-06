8e776342e4464c5f4765de3f02752da358e6c6a9
Dmytro Voloshyniuk <dvoloshinyuk@gmail.com>
2021-08-06 18:18:52 +0300

    Code cleanup

--- airflow/dags/move_files.py
+++ airflow/dags/move_files.py
@@ -17,23 +17,17 @@ default_args = {
 }
 today = datetime.datetime.today() 
 yesterday = date.today() - timedelta(days=1)
-# print ('Current date and time:', d)
  
 def _file_mover(src_folder , dst_folder):
-    # Search files with .txt extension in source directory
     file_names = os.listdir(src_folder)
 
     for file_name in file_names:
         shutil.move(os.path.join(src_folder, file_name), dst_folder)
 
-#Converting date into YYYY-MM-DD format
-#print(d.strftime('%Y-%m-%d'))
-#we need yesterday and today date formats, but prefix and suffix are the same in our example.
-
-with airflow.DAG( "file_sensor_example", default_args= default_args, schedule_interval= "@once"  ) as dag:
-    start_task  = DummyOperator(  task_id= "start" )
-    stop_task   = DummyOperator(  task_id= "stop"  )
-    sensor_task = FileSensor( task_id= "file_sensor_task", poke_interval= 30,  filepath= "/bitnami/new_images/" )
+with airflow.DAG( "mlops_file_pipeline", default_args= default_args, schedule_interval= "@once"  ) as dag:
+    start_task  = DummyOperator(task_id="start")
+    stop_task   = DummyOperator(task_id="stop")
+    sensor_task = FileSensor(task_id="file_sensor_task", poke_interval=30,  filepath="/bitnami/new_images/"])
     put_file    = PythonOperator(task_id='move_new_images', 
                                  python_callable=_file_mover, 
                                  op_args=['/bitnami/new_images/', '/bitnami/ml_image_processing/']
