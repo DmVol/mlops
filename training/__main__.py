a4a9604ad8913f009ac6f156801033791137d20a
Dmytro Voloshyniuk <dvoloshinyuk@gmail.com>
2021-08-23 12:49:38 +0300

    Integrated auto logging for fastai training script

--- training/__main__.py
+++ training/__main__.py
@@ -8,41 +8,29 @@ from mlflow.entities.run import Run
 
 logging.basicConfig(level=logging.INFO)
 
+def print_auto_logged_info(r):
+    tags = {k: v for k, v in r.data.tags.items() if not k.startswith("mlflow.")}
+    artifacts = [f.path for f in MlflowClient().list_artifacts(r.info.run_id, "model")]
+    print("run_id: {}".format(r.info.run_id))
+    print("artifacts: {}".format(artifacts))
+    print("params: {}".format(r.data.params))
+    print("metrics: {}".format(r.data.metrics))
+    print("tags: {}".format(tags))
+
 def main():
 
-    
-    mlfclient = mlflow.tracking.MlflowClient(tracking_uri="http://mlops_mlflow-server_1:5000")
-    mlfexp = mlfclient.create_experiment("training-pets1")
-    run34 = mlfclient.create_run(experiment_id=mlfexp)
+    remote_server_uri = "http://mlops_mlflow-server_1:5000"
+    mlflow.set_tracking_uri(remote_server_uri)
 
     params = {
     'size': 224,
     'epochs': 4,
-    'bs': 64
+    'bs': 8
     }
 
-    for k,v in params.items():
-        mlfclient.log_param(run_id=run34.info.run_uuid, key=k, value=v)
+    mlflow.set_experiment("/my-experiment")
 
-
-    class MLFlowTracking(LearnerCallback):
-        "A `LearnerCallback` that tracks the loss and other metrics into MLFlow"
-        def __init__(self, learn:Learner, client:MlflowClient, run_id: Run):
-            super().__init__(learn)
-            self.learn = learn
-            self.client = client
-            self.run_id = run_id
-            self.metrics_names = ['train_loss', 'valid_loss'] + [o.__name__ for o in learn.metrics]
-
-        def on_epoch_end(self, epoch, **kwargs:Any)->None:
-            "Send loss and other metrics values to MLFlow after each epoch"
-            if kwargs['smooth_loss'] is None or kwargs["last_metrics"] is None:
-                return
-            metrics = [kwargs['smooth_loss']] + kwargs["last_metrics"]
-            for name, val in zip(self.metrics_names, metrics):
-                self.client.log_metric(self.run_id, name, np.float(val))
-    
-    path_img = '/training/data'
+    path_img = '/training/catsdogsrabbits'
     fnames = get_image_files(path_img)
     np.random.seed(2)
     pat = r'/([^/]+)_\d+.jpg$'
@@ -51,20 +39,21 @@ def main():
 
     data = ImageDataBunch.from_name_re(path_img, fnames, pat, ds_tfms=get_transforms(), size=params['size'], bs=params['bs']).normalize(imagenet_stats)
 
-    learn = cnn_learner(data, models.resnet34, metrics=error_rate, callback_fns=[partial(MLFlowTracking, client=mlfclient, run_id=run34.info.run_uuid)])
+    learn = cnn_learner(data, models.resnet34, metrics=error_rate)
 
     logging.info("loading latest parameters")
 
-    learn.load('/training//models/best_resnet34',strict=False,remove_module=True)
+    learn.load('/training/models/catsdogsrabbits_resnet34',strict=False,remove_module=True)
 
     logging.info("start fit")
 
-    learn.fit_one_cycle(params['epochs'])
+    with mlflow.start_run() as run:
+        learn.fit_one_cycle(params['epochs'])
 
     logging.info("end fit")
 
-    learn.save('/models/stage-1')
+    learn.save('stage-1')
 
-    mlfclient.set_terminated(run_id=run34.info.run_uuid)
+    print_auto_logged_info(mlflow.get_run(run_id=run.info.run_id))
 
 main()
